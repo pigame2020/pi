b06a4765d7f8666510aae95324cd910f9ed4094f
pigame2020 <pigame@protonmail.com>
2020-03-06 16:09:27 +0800

    update

---
+++ docs/precache-manifest.58a710ba5f2abfeb415f55c518fb5d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01c8b4e69593ec3c124055b3478edf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11272baaa4206d5b75d",
+    "url": "/static/css/2.145688f1.chunk.css"
+  },
+  {
+    "revision": "b833a51abc5693634591",
+    "url": "/static/css/main.5269c5ad.chunk.css"
+  },
+  {
+    "revision": "c11272baaa4206d5b75d",
+    "url": "/static/js/2.6317210f.chunk.js"
+  },
+  {
+    "revision": "b833a51abc5693634591",
+    "url": "/static/js/main.8427213c.chunk.js"
+  },
+  {
+    "revision": "60387fa30ecc6520b460",
+    "url": "/static/js/runtime~main.584203d5.js"
+  },
+  {
+    "revision": "936f9807dea823c814a2898682ea1195",
+    "url": "/static/media/logo.936f9807.png"
+  }
+]);
