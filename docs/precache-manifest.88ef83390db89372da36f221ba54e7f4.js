9184f9d78f9cccd674212f81a62519f3254f140d
pigame2020 <pigame@protonmail.com>
2020-03-06 16:16:33 +0800

    update

---
+++ docs/precache-manifest.88ef83390db89372da36f221ba54e7f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79a0fb943009e3b7c52420fe6d583d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70cb430a9fb8a4a20b2b",
+    "url": "./static/css/2.145688f1.chunk.css"
+  },
+  {
+    "revision": "50e868e1ad5975e0c139",
+    "url": "./static/css/main.5269c5ad.chunk.css"
+  },
+  {
+    "revision": "70cb430a9fb8a4a20b2b",
+    "url": "./static/js/2.382617df.chunk.js"
+  },
+  {
+    "revision": "50e868e1ad5975e0c139",
+    "url": "./static/js/main.d9a762d1.chunk.js"
+  },
+  {
+    "revision": "14210bba15845dc099b8",
+    "url": "./static/js/runtime~main.46fc1e03.js"
+  },
+  {
+    "revision": "936f9807dea823c814a2898682ea1195",
+    "url": "./static/media/logo.936f9807.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58a710ba5f2abfeb415f55c518fb5d73.js"
+  "./precache-manifest.88ef83390db89372da36f221ba54e7f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
